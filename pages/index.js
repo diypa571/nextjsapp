8a825e49cecc74002d948fa67b79e8513f031573
Diyar Parwana <diypa571@gmail.com>
2022-08-24 09:52:51 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -302,10 +302,10 @@ export default function Home() {
                     </div>
                     <div className="card-body">
                       <h5 className="card-title">Card title</h5>
-                      <p className="card-text">
-                        Some quick example text to build on the card title and
-                        make up the bulk of the card's content.
-                      </p>
+                    <div className="card-text">
+                    Some quick example text to build on the card title and
+                    make up the bulk of the cards content.
+                    </div>
                       <a href="#!" className="btn btn-primary">
                         Button
                       </a>
@@ -335,7 +335,7 @@ export default function Home() {
                       <h5 className="card-title">Card title</h5>
                       <p className="card-text">
                         Some quick example text to build on the card title and
-                        make up the bulk of the card's content.
+                        make up the bulk of the cards content.
                       </p>
                       <a href="#!" className="btn btn-primary">
                         Button
@@ -366,7 +366,7 @@ export default function Home() {
                       <h5 className="card-title">Card title</h5>
                       <p className="card-text">
                         Some quick example text to build on the card title and
-                        make up the bulk of the card's content.
+                        make up the bulk of the cards content.
                       </p>
                       <a href="#!" className="btn btn-primary">
                         Button
@@ -583,13 +583,10 @@ export default function Home() {
     </div>
   </footer>
 
-
-
-
-
-    <script type="text/javascript" src="js/mdb.min.js"></script>
-    <script src=" https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js " integrity=" sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo " crossorigin=" anonymous "></script>
-    <script src=" https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js " integrity=" sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/ " crossorigin=" anonymous "></script>
+  
+<script src="js/mdb.min.js" async></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" async></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" async></script>
 
     
 </>
